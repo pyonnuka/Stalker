0d9a45bfbf2d4b515b4feb000ab2acd70bb0de64
masaki_nukaga <masaki.nukaga1209@gmail.com>
2016-08-27 16:52:40 +0900

    add view files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    redirect_to :login unless current_user
+  end
+
+  def login
+    redirect_to :index if current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get 'artist/:id/ranking', to: 'artists#ranking'
+  get 'artist/:id/map', to: 'artists#map'
 
   root 'home#index'
 end
