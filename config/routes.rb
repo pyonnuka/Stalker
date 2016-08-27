576ed5bf7edbb73967a3acd24a02fa1f8ee39c8a
masaki_nukaga <masaki.nukaga1209@gmail.com>
2016-08-28 03:55:39 +0900

    add real time distance

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  get 'artist/:id', to: 'artists#show'
+  resources :artists, :only => [:show]
 
   root 'home#index'
   get 'login', to: 'home#login'
