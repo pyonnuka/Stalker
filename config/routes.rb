abd3899eb5d9e18e2313cfe7687730ebf7e870c9
HOkn <haruokny@gmail.com>
2016-08-28 08:13:00 +0900

    [add] artist index pg

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   get 'artist/:id', to: 'artists#show'
+  get 'artist', to: 'artists#index'
 
   root 'home#index'
   get 'login', to: 'home#login'
