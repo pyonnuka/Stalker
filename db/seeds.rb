7a87ce886a2e19f018e911749512d8323346ea30
masaki_nukaga <masaki.nukaga1209@gmail.com>
2016-08-28 00:02:19 +0900

    add position seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,4 @@ Artist.create(id:'2', name:'はるちゃん2')
 Artist.create(id:'3', name:'はるちゃん3')
 Artist.create(id:'4', name:'はるちゃん')
 
+Artist.find(2).positions << Position.create(name: "渋谷WWW", event_title: "ライブ", event_url: "http://twitter.com", in_there_at: "2016-8-28")
