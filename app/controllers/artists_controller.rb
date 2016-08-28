8ac7d90f23658704755502856418a7fe796b7248
masaki_nukaga <masaki.nukaga1209@gmail.com>
2016-08-28 14:53:13 +0900

    delete useless logic

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,9 +3,6 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
     artist_positions = Position.find_by(artist_id: "#{@artist.id}")
     gon.place = artist_positions.name
-    @checkins_order_by_ranking = Checkin.where(artist: @artist).sort_by do |checkin|
-      checkin.distance
-    end
   end
 
   def index
