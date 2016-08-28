f516bc30e68f1f9fec98119341fe31f5f2fbdb52
masaki_nukaga <masaki.nukaga1209@gmail.com>
2016-08-28 15:12:28 +0900

    fix conflict

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,9 +2,6 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
     gon.position = @artist.positions.where.not(name: nil).map{ |p| p.name }
-    @checkins_order_by_ranking = Checkin.where(artist: @artist).sort_by do |checkin|
-      checkin.distance
-    end
   end
 
   def index
