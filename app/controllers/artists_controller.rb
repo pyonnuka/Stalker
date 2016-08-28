a4a08c70fefdc70c84c027a7779cbfde9d399599
Yoshitomo Yasuno <yasunodayo@yahoo.com>
2016-08-28 11:39:43 +0900

    finished location

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,7 @@
 class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
-    artist_positions = Position.find_by(artist_id: "#{@artist.id}")
-    gon.place = artist_positions.name
+    gon.positions = @artist.positions.map{ |p| p.name }
     @checkins_order_by_ranking = Checkin.where(artist: @artist).sort_by do |checkin|
       checkin.distance
     end
